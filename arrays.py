f0e616796f1fd088f08cbecae574fbb4281fc84c
Evans <kiropevans097@gmail.com>
2024-04-10 14:08:37 +0300

    math,statements,array

---
+++ arrays.py
@@ -0,0 +1,20 @@
+data = [1.6,3.4,5.5,9.4]
+#getting all values in array
+for x in data:
+    print(x)
+#getting length of an array
+N = len(data)
+print(N)
+#getting a specific value inside an array
+#lets get index = 2
+x = data[2]
+print(x)
+#adding a element to an array using append
+data.append(11.4)
+print(data)
+#adding an alement using insert at a specific index 2
+data.insert(2,20.3)
+print(data)
+#counting elements in an array
+
+
