d9c79b51aadcd7e2572b90029ab0c5660481b882
Evans <kiropevans097@gmail.com>
2024-04-10 15:56:35 +0300

    added for loop

---
+++ forloop.py
@@ -0,0 +1,26 @@
+#getting all range
+#for i in range (1,10):
+    #print(i)
+#getting all data
+    #data = [1.6,3.4,5.5,8,9,7,4]
+    #for x in data:
+        #print(x)
+#getting all cars in the carlist
+#carlist = ["Bens","Volvo","Toyota","Tesla"]
+#for car in carlist:
+     #print(car)
+
+#N = 10
+#for x in range(N):
+   # print(x)
+#USING FOR LOOPS FOR DATA SUMMATION.
+data = [1,5,6,3,12,3]
+#sum = 0
+#for x in data:
+    #sum = sum + x
+    #print(sum)
+N = len(data)
+sum_of_elements = sum(data)
+mean = sum_of_elements / N
+print(mean)
+
